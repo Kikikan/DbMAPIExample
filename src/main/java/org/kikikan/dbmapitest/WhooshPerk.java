9420172596768c49b0526fb0a9b4d1eebf7fe9df
Kikikan <gykrisz00@gmail.com>
2020-08-30 12:51:24 +0200

    Updated for release 1.0.1

--- src/main/java/org/kikikan/dbmapitest/WhooshPerk.java
+++ src/main/java/org/kikikan/dbmapitest/WhooshPerk.java
@@ -4,19 +4,22 @@ import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
+import org.kikikan.deadbymoonlight.cooldowns.CooldownController;
+import org.kikikan.deadbymoonlight.cooldowns.CustomCooldown;
 import org.kikikan.deadbymoonlight.events.player.both.VaultEvent;
+import org.kikikan.deadbymoonlight.events.player.survivor.modifiableevents.GetScratchMarkDurationEvent;
 import org.kikikan.deadbymoonlight.game.PerkUser;
 import org.kikikan.deadbymoonlight.game.Survivor;
 import org.kikikan.deadbymoonlight.perks.CooldownPerk;
 
 @SuppressWarnings({"unused"})
-public final class WhooshPerk extends CooldownPerk {
-
-    private int originalScratchMarkLength = 0;
+public final class WhooshPerk extends CooldownPerk { // No need to implement CooldownController, as CooldownPerk has already implemented it.
 
     public WhooshPerk(JavaPlugin plugin, PerkUser p) {
         super(plugin, p, 1200, false);
     }
+
+    private CustomCooldown scratchMarkTimer = new CustomCooldown(getPlugin(), this, 100);
 
     public boolean isKiller() {
         return true;
@@ -50,24 +53,29 @@ public final class WhooshPerk extends CooldownPerk {
 
     public void onFastVault(VaultEvent event){
         if (!isTurnedOn() && event.player.equals(getPerkUser())){
-            on();
+            on(); // Turns on the Cooldown.
             DeadByMoonlightAPITest pl = (DeadByMoonlightAPITest)getPlugin();
+            pl.whoosh(getPerkUser().getPlayer()); // Uses a method from the main class.
             getPerkUser().getPlayer().addPotionEffect(new PotionEffect(PotionEffectType.INVISIBILITY, 100, 0));
             if (getPerkUser() instanceof Survivor){
-                Survivor survivor = (Survivor)getPerkUser();
-                originalScratchMarkLength = survivor.scratchMarkDuration;
-                survivor.scratchMarkDuration = 0;
+                Survivor s = (Survivor)getPerkUser();
+                s.resetScratchMarkDuration(); // Forces GetScratchMarkDurationEvent to be called.
+                scratchMarkTimer.on(); // Turns on the scratch mark timer, which is set to be turned off after 100 ticks = 5 seconds.
             }
         }
     }
 
     @Override
-    protected void onCooldownOff() {
-        if (getPerkUser() instanceof Survivor){
+    protected void onCooldownOff(CustomCooldown cooldown) {
+        if (cooldown == scratchMarkTimer && getPerkUser() instanceof Survivor){
             Survivor survivor = (Survivor)getPerkUser();
-            survivor.scratchMarkDuration = originalScratchMarkLength;
-            DeadByMoonlightAPITest pl = (DeadByMoonlightAPITest)getPlugin();
-            pl.whoosh(survivor.getPlayer());
+            survivor.resetScratchMarkDuration(); // Forces GetScratchMarkDurationEvent to be called.
+        }
+    }
+
+    public void onScratchMarkEvent(GetScratchMarkDurationEvent event){
+        if (scratchMarkTimer.isRunning()){
+            event.setValue(0);
         }
     }
 }
